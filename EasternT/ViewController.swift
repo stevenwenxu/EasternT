1b1a664cd926f5cff1286984b6890c5d40fa1046
Steven Xu <steven.xu13@gmail.com>
2016-09-16 21:07:00 -0400

    Initial Commit

---
+++ EasternT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasternT
+//
+//  Created by Steven Xu on 2016-09-16.
+//  Copyright © 2016 EasternT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
