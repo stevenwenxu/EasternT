f2d4c448ddb15fa95d5e84260b84e9f9f5fc8c4a
Weijie Wang <w268wang@uwaterloo.ca>
2016-09-17 04:04:04 -0400

    add text to voice for all 3 languages

--- EasternT/ETBrain.swift
+++ EasternT/ETBrain.swift
@@ -15,11 +15,31 @@ class ETBrain : NSObject {
         return nil
     }
 
-    func textToSpeech() {
-
+    static func textToSpeech(text: String, languageType: LanguageType) {
+        var languageTypeString: String?
+        
+        switch languageType {
+        case LanguageType.english:
+            languageTypeString = "en-US"
+        case LanguageType.german:
+            languageTypeString = "de-DE"
+        case LanguageType.chinese:
+            languageTypeString = "zh-CN"
+        default:
+            NSLog("Fatal: undefined language type")
+            return
+        }
+        
+        let synth = AVSpeechSynthesizer()
+        
+        let myUtterance = AVSpeechUtterance(string: text)
+        myUtterance.rate = 0.5
+        myUtterance.voice = AVSpeechSynthesisVoice(language: languageTypeString)
+        synth.speak(myUtterance)
     }
 
     func translate(from: String) -> String {
+        let translateApiUrl = "https://api.datamarket.azure.com/data.ashx/Bing/microsofttranslatorspeech/v1/Translation"
         return ""
     }
 }
