8865e4acd11e63265d5e8ebc6f718243a5e8dd25
Chengyi Zhou <c44zhou@uwaterloo.ca>
2016-09-18 08:52:58 -0400

    socketIo stuffs

--- EasternT/SocketIOManager.swift
+++ EasternT/SocketIOManager.swift
@@ -10,81 +10,25 @@ import Foundation
 import SocketIO
 
 class SocketIOManager: NSObject {
-    static let sharedInstance = SocketIOManager()
-    
-    var socket: SocketIOClient = SocketIOClient(socketURL: URL(string: "https://ece.uwaterloo.ca/~c44zhou/")!)
-    
-    
-    override init() {
-        super.init()
-    }
-    
-    
-    func establishConnection() {
+    func getConnected() {
+        let socket = SocketIOClient(socketURL: URL(string: "https://easternt-3a493.firebaseapp.com")!, config: [.log(true), .forcePolling(true)])
+        
+        socket.on("connect") {data, ack in
+            print("socket connected")
+        }
+        
+        socket.on("currentAmount") {data, ack in
+            if let cur = data[0] as? Double {
+                socket.emitWithAck("canUpdate", cur)(0) {data in
+                    socket.emit("update", ["amount": cur + 2.50])
+                }
+                
+                ack.with("Got your currentAmount", "dude")
+            }
+        }
+        
         socket.connect()
+
     }
     
-    
-    func closeConnection() {
-        socket.disconnect()
-    }
-    
-    
-    func connectToServerWithNickname(nickname: String, completionHandler: @escaping (_ userList: [[String: AnyObject]]?) -> Void) {
-        socket.emit("connectUser", nickname)
-        
-        socket.on("userList") { ( dataArray, ack) -> Void in
-            completionHandler(dataArray[0] as! [[String: AnyObject]])
-        }
-        
-        listenForOtherMessages()
-    }
-    
-    
-    func exitChatWithNickname(nickname: String, completionHandler: () -> Void) {
-        socket.emit("exitUser", nickname)
-        completionHandler()
-    }
-    
-    
-    func sendMessage(message: String, withNickname nickname: String) {
-        socket.emit("chatMessage", nickname, message)
-    }
-    
-    
-    func getChatMessage(completionHandler: @escaping (_ messageInfo: [String: AnyObject]) -> Void) {
-        socket.on("newChatMessage") { (dataArray, socketAck) -> Void in
-            var messageDictionary = [String: AnyObject]()
-            messageDictionary["nickname"] = dataArray[0] as! String as AnyObject?
-            messageDictionary["message"] = dataArray[1] as! String as AnyObject?
-            messageDictionary["date"] = dataArray[2] as! String as AnyObject?
-            
-            completionHandler(messageDictionary)
-        }
-    }
-    
-    
-    private func listenForOtherMessages() {
-        socket.on("userConnectUpdate") { (dataArray, socketAck) -> Void in
-            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "userWasConnectedNotification"), object: dataArray[0] as! [String: AnyObject])
-        }
-        
-        socket.on("userExitUpdate") { (dataArray, socketAck) -> Void in
-            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "userWasDisconnectedNotification"), object: dataArray[0] as! String)
-        }
-        
-        socket.on("userTypingUpdate") { (dataArray, socketAck) -> Void in
-            NotificationCenter.default.post(name: NSNotification.Name(rawValue: "userTypingNotification"), object: dataArray[0] as? [String: AnyObject])
-        }
-    }
-    
-    
-    func sendStartTypingMessage(nickname: String) {
-        socket.emit("startType", nickname)
-    }
-    
-    
-    func sendStopTypingMessage(nickname: String) {
-        socket.emit("stopType", nickname)
-    }
 }
